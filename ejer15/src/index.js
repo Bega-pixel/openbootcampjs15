4884c3eb177a7ee6300cdfcd255c6b072c2ca7b6
Bega-pixel <bega2000@protonmail.com>
2022-11-11 16:51:59 +0100

    ejer15

---
+++ ejer15/src/index.js
@@ -0,0 +1,17 @@
+const nombre = "Cobi";
+const apellido = "Olimpic";
+
+const persona = {
+    nombre,
+    apellido
+}
+
+//localStorage.setItem("persona", JSON.stringify({nombre, apellido}))
+//localStorage.removeItem("persona", JSON.stringify({nombre, apellido}))
+
+//sessionStorage.setItem("perona",JSON.stringify({nombre, apellido}))
+
+//const fecha = new Date()
+//document.cookie = `persona=${JSON.stringify(persona)};expires=${new Date(fecha.getTime() + 2 * 60000)}`
+
+
